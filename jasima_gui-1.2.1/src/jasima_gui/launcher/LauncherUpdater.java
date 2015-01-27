a928675e0803534608e62eea29606aa34d65b1e2
thildebrandt <THildebrandt@gmail.com>
2015-01-27 08:52:10 +0000

    version for release 1.2.1, 2015-01-27

---
+++ jasima_gui-1.2.1/src/jasima_gui/launcher/LauncherUpdater.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2015 Torsten Hildebrandt and jasima contributors
+ *
+ * This file is part of jasima, v1.2.
+ *
+ * jasima is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * jasima is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jasima.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package jasima_gui.launcher;
+
+import java.util.ArrayList;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
+
+public class LauncherUpdater implements IResourceChangeListener, IResourceDeltaVisitor {
+
+	public LauncherUpdater() {
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(this);
+	}
+
+	public void dispose() {
+		ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
+	}
+
+	@Override
+	public void resourceChanged(IResourceChangeEvent event) {
+		IResourceDelta delta = event.getDelta();
+		try {
+			delta.accept(this);
+		} catch (CoreException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public boolean visit(IResourceDelta delta) throws CoreException {
+		IPath movedTo = delta.getMovedToPath();
+		if (movedTo != null) {
+			ILaunchConfiguration config = SimulationLaunchShortcut.findConfiguration(delta.getResource());
+			if (config != null) {
+				ILaunchConfigurationWorkingCopy wc = config.getWorkingCopy();
+				ArrayList<String> args = Arguments.parseArgs(wc);
+				Arguments outArgs = new Arguments();
+				IFile newFile = ResourcesPlugin.getWorkspace().getRoot().getFile(movedTo);
+				boolean argFound = false;
+				for (String arg : args) {
+					if (argFound) {
+						outArgs.append(arg);
+					} else if (arg.startsWith("--")) {
+						outArgs.append(arg);
+					} else {
+						argFound = true;
+						outArgs.append(newFile.getProjectRelativePath().toString());
+					}
+				}
+				wc.setMappedResources(new IResource[] { newFile });
+				wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS, outArgs.toString());
+				wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, newFile.getProject().getName());
+				wc.doSave();
+			}
+		} else if (delta.getKind() == IResourceDelta.REMOVED) {
+			ILaunchConfiguration config = SimulationLaunchShortcut.findConfiguration(delta.getResource());
+			if (config != null) {
+				config.delete();
+			}
+		}
+		return true;
+	}
+}
