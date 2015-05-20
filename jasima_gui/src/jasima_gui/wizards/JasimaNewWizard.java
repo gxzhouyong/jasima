0c43470b7e1444d0442819a2b7e593501d8de334
r.kreis <r.kreis@uni-bremen.de>
2015-05-20 07:01:09 +0000

    update javadoc

--- jasima_gui/src/jasima_gui/wizards/JasimaNewWizard.java
+++ jasima_gui/src/jasima_gui/wizards/JasimaNewWizard.java
@@ -46,15 +46,6 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
 
-/**
- * This is a sample new wizard. Its role is to create a new file resource in the
- * provided container. If the container resource (a folder or a project) is
- * selected in the workspace when the wizard is opened, it will accept it as the
- * target container. The wizard creates one file with the extension "jasima". If
- * a sample multi-page editor (also available as a template) is registered for
- * the same extension, it will be able to open it.
- */
-
 public class JasimaNewWizard extends Wizard implements INewWizard {
 	private JasimaNewWizardPage page;
 	private ISelection selection;
